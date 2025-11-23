ee15fe7fd356f8fe4fba9f698f4128552c58cab0
BUNYOD YOKUBOV <117659225+Bunyod-Y@users.noreply.github.com>
2025-11-23 13:40:31 +0500

    Enhance SQL script with additional queries
    Added queries to count books and calculate average price by year.

--- task1/sql_query.sql
+++ task1/sql_query.sql
@@ -1,5 +1,3 @@
-#########################################################################
-
 CREATE TABLE books_raw (
     id NUMERIC(20,0) PRIMARY KEY,
     title TEXT,
@@ -7,10 +5,16 @@ CREATE TABLE books_raw (
     price_eur NUMERIC(10,2) 
 )
 
-##########################################################################
-  
 COPY books_raw FROM 'D:\books_data.csv' CSV HEADER;
 
-##########################################################################
-
 SELECT * FROM books_raw;
+
+SELECT year, COUNT(*) as num_of_books_in_this_year
+FROM books_raw
+GROUP BY year
+ORDER BY year;
+
+SELECT year, AVG(price_eur) as average_price
+FROM books_raw
+GROUP BY year
+ORDER BY year;
